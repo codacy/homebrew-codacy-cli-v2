5f7b98b47b51609f3136e6771ed1fd1b88436f37
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-05-22 18:29:17 +0200

    Add v2 to the name

--- Formula/codacy-cli-v2.rb
+++ Formula/codacy-cli-v2.rb
@@ -4,6 +4,6 @@ class CodacyCliV2 < Formula
   sha256 "fb616e2f5c639985566c81a6e6ce51db2e8de56bf217e837d13efe2f3ccc3042"
 
   def install
-    bin.install "codacy-cli.sh" => "codacy-cli"
+    bin.install "codacy-cli.sh" => "codacy-cli-v2"
   end
 end
